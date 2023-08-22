665897b24bee99b5dd71649599cfa1e14da1f32b
nimscodes <princenimako28@gmail.com>
2023-08-22 14:32:41 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="p-5">
+     <h1 className="font-bold text-3xl text-gun-metal font-pacifico">
+      nims<span className="text-gold-metallic">codes</span>
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'gun-metal': '#283141',
+        'gold-metallic': '#D9B861',
+        'persian-green': '#01A78F',
+        'blue-munsell': '#3487A3',
+        'tea-rose-red': '#DDAFB3',
+      },
+      fontFamily: {
+        'pacifico': ['Pacifico', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
