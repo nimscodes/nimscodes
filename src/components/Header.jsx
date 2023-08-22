0400ca5adfff24e394c4c0411edc3d716bab1e02
nimscodes <princenimako28@gmail.com>
2023-08-22 18:51:08 +0100

    mobile nav build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
+import Logo from './Logo';
+import MobileNav from './MobileNav';
 import ModeSwitcher from './ModeSwitcher';
 
 const Header = ({ darkMode, onToggleMode }) => {
   return (
     <div className="flex items-center justify-between">
-      <h1 className="font-bold text-3xl font-pacifico">
-        nims<span className="text-gold-metallic">codes</span>
-      </h1>
+      <Logo />
       <ModeSwitcher darkMode={darkMode} onToggleMode={onToggleMode} />
+      <MobileNav />
     </div>
   );
 };
