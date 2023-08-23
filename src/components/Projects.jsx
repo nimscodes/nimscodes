cbce5d1fc1614b85d1adff7cf9de6a204bac870e
nimscodes <princenimako28@gmail.com>
2023-08-23 18:39:24 +0100

    projects halfway through

---
+++ src/components/Projects.jsx
@@ -0,0 +1,24 @@
+import data from '../data.json'
+import ProjectCard from './ProjectCard';
+
+const Projects = () => {
+
+  const renderedProjects = data.map(project => {
+    return <ProjectCard key={project.id} data={project} />
+  })
+  
+  return (
+    <div className='flex flex-col gap-5 my-[50px]'>
+      <div className='space-y-3'>
+        <h3 className='text-xs text-gray-500'>— LATEST WORK</h3>
+        <h1 className='text-4xl'>Projects </h1>
+      </div>
+      <div className='flex gap-20 flex-wrap'>
+        {renderedProjects}
+      </div>
+      
+    </div>
+  )
+}
+
+export default Projects
