b1106e0cf9cb5db4d451b0279411039f9eb27f52
nimscodes <princenimako28@gmail.com>
2023-08-23 14:30:04 +0100

    some design touches to hero

--- src/components/LeftHero.jsx
+++ src/components/LeftHero.jsx
@@ -3,7 +3,8 @@ import Download from './Download';
 
 const LeftHero = () => {
   return (
-    <div className="md:w-[48%] flex flex-col justify-end gap-3 mb-5">
+    <div className=" relative md:w-[48%] flex flex-col justify-end gap-3 mb-5">
+      <div className='absolute w-60 h-60 transform rotate-45 shadow-2xl top-[50%] left-[10%] '></div>
       <span className="text-xs md:text-md text-gray-400 dark:text-gray-600">
         —— MY NAME IS
       </span>
@@ -11,7 +12,7 @@ const LeftHero = () => {
         Prince <span className="text-gold-metallic">Nimako</span>
       </span>
       <span className="font-pacifico text-3xl md:text-4xl text-gray-500">
-        a Frontend Developer
+        a Frontend Web Developer
       </span>
       <p className="text-sm mt-4 w-[85%] md:w-[70%]">
         Based in the UK, I am a front-end developer passionate about building
