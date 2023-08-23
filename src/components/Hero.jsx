9248b5016882dda6e5b2b352b7ee820b3a6c66fb
nimscodes <princenimako28@gmail.com>
2023-08-23 19:00:24 +0100

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,6 @@
 import LeftHero from './LeftHero';
 import RightHero from './RightHero';
 
-
-import './hero.css'
-
 const Hero = ({ isMobile }) => {
   return (
     <div className="flex flex-col md:flex-row py-10 md:py-0 justify-between mt-10 md:mt-0">
