0d1b20bea661938a1679c5125af3abc529d50f41
nimscodes <princenimako28@gmail.com>
2023-09-01 16:32:58 +0100

    modify Download component

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -2,13 +2,13 @@ import { FaDownload } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
 const Download = () => {
-  const resumeFilePath = '/resume/resume1.pdf';
+  const resumeFilePath = '/resume/resume-fed.pdf';
   return (
     <motion.a
       whileHover={{ scale: 1.1 }}
       transition={{ type: 'spring', stiffness: 400, damping: 10 }}
       href={resumeFilePath}
-      download="resume1.pdf"
+      download="resume-fed.pdf"
       className="cursor-pointer flex items-center justify-center py-3 px-6 gap-2 border-b shadow-xl border-gun-metal "
     >
       <span>Download Resume</span>
